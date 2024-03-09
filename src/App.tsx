9ec5ec5c76f3a3db8f38247a64331883d25ec36b
infinite1400 <muraripandey1886@gmail.com>
2024-03-10 00:23:39 +0530

    added note page

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,36 @@ import { Routes, Route, Navigate } from "react-router-dom";
 // components 
 import NewNote from "./NewNote";
 
+// Types 
+export type Note = {
+  id : string
+} & NoteData
+
+export type RowNote={
+  id : string
+}
+
+export type RowNoteData={
+  title : string,
+  markdown : string
+  tagIds : string[]
+}
+
+export type NoteData={
+  title : string,
+  markdown : string
+  tags : Tag[]
+}
+
+export type Tag={
+  id : string
+  label : string
+}
+
+
 const App = () => {
+  const [notes,setNotes]useLocalStorage<RawNote[]>("NOTES",[]);
+  const [tags,setTags]useLocalStorage<Tag[]>("TAGS",[]);
   return (
     <Container className="my-4">
       <Routes>
--- src/NoteForm.tsx
+++ src/NoteForm.tsx
@@ -1,32 +1,64 @@
-import React from 'react'
+import React, { FormEvent, useRef, useState } from 'react'
 import { Button, Col, Form, Row, Stack } from 'react-bootstrap'
-import { Link, Navigate, useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import CreatableReactSelect from "react-select/creatable";
-const NoteForm = () => {
+import { NoteData, Tag } from './App';
+
+
+type NoteFormProps = {
+    onSubmit: (data: NoteData) => void
+}
+
+const NoteForm = ({ onSubmit }: NoteFormProps) => {
     const navigate = useNavigate();
+    const titleRef = useRef<HTMLInputElement>(null);
+    const markdownRef = useRef<HTMLTextAreaElement>(null);
+    const [selectedTags, setSelectedTags] = useState<Tag[]>([]);
     const previousPage = () => {
         navigate('..');
     }
+
+    const handleSubmit = (event: FormEvent) => {
+        event.preventDefault();
+        onSubmit({
+            // Type narrowing of string | undefined type to string by two methods 
+            title: titleRef.current?.value as string,
+            markdown: markdownRef.current?.value ?? "",
+            tags: [].
+        })
+    }
     return (
-        <Form>
+        <Form onSubmit={handleSubmit} >
             <Stack gap={4}>
                 <Row>
                     <Col>
                         <Form.Group controlId='title'>
                             <Form.Label>Title</Form.Label>
-                            <Form.Control required />
+                            <Form.Control ref={titleRef} required />
                         </Form.Group>
                     </Col>
                     <Col>
                         <Form.Group controlId='tags'>
                             <Form.Label>Tags</Form.Label>
-                            <CreatableReactSelect isMulti />
+                            <CreatableReactSelect
+                                value={selectedTags.map(tag => {
+                                    return { label: tag.label, value: tag.id }
+                                })}
+                                onChange={tags => {
+                                    setSelectedTags(
+                                        tags.map(tag => {
+                                            return { label: tag.label, id: tag.value }
+                                        })
+                                    )
+                                }}
+
+                                isMulti />
                         </Form.Group>
                     </Col>
                 </Row>
                 <Form.Group controlId='markdowm'>
                     <Form.Label>Body</Form.Label>
-                    <Form.Control required as="textarea" rows={15} />
+                    <Form.Control ref={markdownRef} required as="textarea" rows={15} />
                 </Form.Group>
                 <Stack direction='horizontal' gap={2} className='justify-content-end'>
                     <Button type='submit' variant='outline-primary' >Save</Button>
